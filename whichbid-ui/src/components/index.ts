f5bbd355337bad79313dd44c35997ecca261b6ae
apondi-art <aiyanarich64@gmail.com>
2026-01-31 00:44:25 +0300

    Update component exports to include new ThemeToggle component

--- whichbid-ui/src/components/index.ts
+++ whichbid-ui/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as CriteriaForm } from "./CriteriaForm";
 export { default as ProcessingIndicator } from "./ProcessingIndicator";
 export { default as ResultsDisplay } from "./ResultsDisplay";
 export { default as DownloadButton } from "./DownloadButton";
+export { ThemeToggle } from "./ThemeToggle";
